41833858853b86a83abb19e6f0a4beebc4fa9cd6
Yousef Elgendy <elgendyyousef539@gmail.com>
2025-02-25 23:03:02 +0200

    Assignment 2

---
+++ PHP/Task_1/Assignment_2/Problem2.php
@@ -0,0 +1,6 @@
+<?php
+// Method one
+echo gettype(100) . "<br>";
+
+# Method two
+var_dump(100) . "<br>";
---
+++ PHP/Task_1/Assignment_2/Problem4.php
@@ -0,0 +1,17 @@
+<?php
+echo nl2br(
+    'We
+    Love
+    Elzero
+    Web
+    School'
+);
+
+// Needed Output
+/*
+We
+Love
+Elzero
+Web
+School
+*/
