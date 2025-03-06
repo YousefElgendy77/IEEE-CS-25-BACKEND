296b4ea587e428ab47e30c182bae2edf4d4c327d
Yousef Elgendy <elgendyyousef539@gmail.com>
2025-03-06 17:12:32 +0200

    Assignment 1

--- PHP/Task_4/Assignment1/Problem1.php
+++ PHP/Task_4/Assignment1/Problem1.php
@@ -1,4 +1,4 @@
 <?php
 
-echo round(disk_total_space("C:") / 1024 / 1024 / 1024 / 1024, 2) . '<br>';  // 0.15
-echo round(disk_total_space("D:") / 1024 / 1024 / 1024 / 1024, 2) . '<br>';  // 0.32
+echo round(disk_total_space("C:") / 1024 / 1024 / 1024 / 1024, 2) . ' Terabyte<br>';  // 0.15
+echo round(disk_total_space("D:") / 1024 / 1024 / 1024 / 1024, 2) . ' Terabyte<br>';  // 0.32
